d8aeb636ca78e5f0ca16e331ac40470bf36e43d1
KoroPutra <putramei05@gmail.com>
2023-06-08 02:39:39 +0700

    Revert "membuat file belajar txt untuk mengingat perintah program"
    This reverts commit 49e8220a5b44f569edb09576b659936f0f6c46b5.

--- auth.js
+++ auth.js
@@ -2,9 +2,13 @@ console.log("Hello World")
 
 const add = (a, b) => a + b;
 console.log(add(1,2));
+<<<<<<< HEAD
 
-const multiplyNumber = (a, b) => a*b;
-console.log(multiplyNumber(2,2))
+const multiply = (a, b) => a*b;
+console.log(multiply(2,2))
 
 const divide = (a, b) => a/b;
 console.log(divide(10,2))
+=======
+console.log("halo bang")
+>>>>>>> parent of 74016cf (menambahkan fitur perkalian)
