386a5ca8a93b80589e55a85d875103f341dfa4ac
KoroPutra <putramei05@gmail.com>
2023-06-08 01:57:09 +0700

    menambahkan fitur pembagian

--- auth.js
+++ auth.js
@@ -3,6 +3,8 @@ console.log("Hello World")
 const add = (a, b) => a + b;
 console.log(add(1,2));
 
-
 const multiply = (a, b) => a*b;
 console.log(multiply(2,2))
+
+const divide = (a, b) => a/b;
+console.log(divide(10,2))
